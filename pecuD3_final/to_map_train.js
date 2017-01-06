509fd2da8aa2b1ba4d1e3c08635aabffd43df887
OOmegaPPanDDa <HSIN@TCGB.m3.ntu.edu.tw>
2017-01-06 23:08:15 +0800

    test

--- pecuD3_final/to_map_train.js
+++ pecuD3_final/to_map_train.js
@@ -2,7 +2,7 @@ function to_map_train(){
 
   var width = 80;
   var height = 50;
-  var margin = {top:5%};
+  var margin = {top:5};
 
 
   d3.csv('https://raw.githubusercontent.com/OOmegaPPanDDa/D3/master/pecuD3_final/2014_train_year_flow.csv',function(stop_data){
